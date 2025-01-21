9077a683e0d2e7394dffb6c7efa5fcaab3420e41
jbssneto.develcode <jbssneto.develcode@rd.com.br>
2025-01-21 15:32:18 -0300

    Dictionarie features_limits

---
+++ Dictionaries/features_limits.py
@@ -0,0 +1,8 @@
+#Valores considerados como normais para cada medição
+features_limits = { 
+    'creatinine_phosphokinase': (32, 294),
+    'ejection_fraction': (40, 60),
+    'platelets': (140000, 440000),
+    'serum_creatinine': (0.6, 1.3), 
+    'serum_sodium': (135, 145),  
+}
