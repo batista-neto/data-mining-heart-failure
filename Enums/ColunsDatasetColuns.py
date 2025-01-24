40412548fefe05f80b794dd3ba6d5f829be3e83d
Brvn0x <bruno.mc@discente.ufma.com.br>
2025-01-24 17:58:57 -0300

    enum ColunsDatasetColuns

---
+++ Enums/ColunsDatasetColuns.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+# Definindo um Enum
+class DatasetColuns(Enum):
+    age = "age"
+    anemia = "anaemia"
+    creatinine_phosphokinase = "creatinine_phosphokinase"
+    diabetes = "diabetes"
+    ejection_fraction = "ejection_fraction"
+    high_blood_pressure = "high_blood_pressure"
+    platelets = "platelets"
+    serum_creatinine = "serum_creatinine"
+    serum_sodium = "serum_sodium"
+    sex = "sex"
+    smoking = "smoking"
+    time = "time"
+    DEATH_EVENT = "DEATH_EVENT"
