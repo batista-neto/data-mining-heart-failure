ea6534e2c3ec94278d17ac11c786fbf87b122740
Brvn0x <bruno.mc@discente.ufma.com.br>
2025-02-13 17:22:53 -0300

    update ModelStrategy

--- Strategies/ModelStrategy.py
+++ Strategies/ModelStrategy.py
@@ -1,4 +1,8 @@
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
 from Enums.ModelsType import ModelsType
 
 class ModelStrategy:
@@ -8,6 +12,12 @@ class ModelStrategy:
         """
         if model_type == ModelsType.LogisticRegression.value:
             model_type = LogisticRegression(solver='liblinear', random_state=42)
+        elif model_type == ModelsType.RandomForest.value:
+            model_type =  RandomForestClassifier(random_state=42)
+        elif model_type == ModelsType.XGBoost.value:
+            model_type =  XGBClassifier(random_state=42)
+        elif model_type == ModelsType.KNN.value:
+            model_type = KNeighborsClassifier()
         else:
             raise ValueError(f"Scaler '{model_type}' não é suportado.")
 
