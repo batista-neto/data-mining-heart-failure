73572a69be616ac515fcf059ac86684bc6aecc70
Brvn0x <bruno.mc@discente.ufma.com.br>
2025-02-13 17:23:35 -0300

    update ModelsType

--- Enums/ModelsType.py
+++ Enums/ModelsType.py
@@ -3,3 +3,6 @@ from enum import Enum
 # Definindo um Enum
 class ModelsType(Enum):
     LogisticRegression = "LogisticRegression"
+    RandomForest = "RandomForest"
+    XGBoost = "XGBoost"
+    KNN = "KNN"
